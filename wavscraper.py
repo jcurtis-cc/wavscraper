e3889f2ea29e4da041731988712f64e21b6bfa1e
Jay Curtis <JCurtisRMIT@users.noreply.github.com>
2019-09-19 21:49:29 +1000

    Update wavscraper.py

--- wavscraper.py
+++ wavscraper.py
@@ -40,10 +40,21 @@ for idx, url in enumerate(scrapedURLs) :
     if os.path.isfile(userpath) :
         print(userpath, " already exists in directory, skipping")
     else:
+        print('\n')
         print("downloading ", idx + 1, " of ", len(scrapedURLs), ": ", url)
-        r = requests.get(url)
-        print("writing: ", userpath)
         with open(userpath, 'wb') as f:
-            f.write(r.content)
-
+            r = requests.get(url, stream=True)
+            total_length = r.headers.get('content-length')
+            if total_length is None:
+                print("writing: ", userpath, '\n')
+                f.write(r.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for data in r.iter_content(chunk_size=4096):
+                    dl += len(data)
+                    f.write(data)
+                    done = int(50 * dl / total_length)
+                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )
+                    sys.stdout.flush()
 
