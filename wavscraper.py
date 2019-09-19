ab4e04aaa480a084078d28c0630238f16bd63216
Jay Curtis <JCurtisRMIT@users.noreply.github.com>
2019-09-19 19:56:57 +1000

    Create wavscraper.py

---
+++ wavscraper.py
@@ -0,0 +1,32 @@
+from lxml import html
+import requests
+import sys
+import os.path
+
+try:
+    URLfromArg = sys.argv[1]
+    userpathFromArg = sys.argv[2]
+except:
+    print("usage: wavscraper.py <url.com> <userpath>")
+    print("eg: wavscraper.py url.com /User/John/Downloads")
+    sys.exit(2)
+
+page = requests.get(URLfromArg)
+webpage = html.fromstring(page.content)
+
+scrapedURLs = webpage.xpath('//a[contains(text(), "wav")]/@href')
+fileName = webpage.xpath('//a[contains(@href, ".wav")]/text()')
+
+for idx, url in enumerate(scrapedURLs) :
+    split = str(url.split("/")[-1:]).strip("['""']")
+    split = split.strip('\"')
+    userpath = userpathFromArg + split
+    if os.path.isfile(userpath) :
+        print(userpath, " already exists in directory, skipping")
+    else:
+        print("downloading ", idx + 1, " of ", len(scrapedURLs), ": ", url)
+        r = requests.get(url)
+        print("writing: ", userpath)
+        with open(userpath, 'wb') as f:
+            f.write(r.content)
+
